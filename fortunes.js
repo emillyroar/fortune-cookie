d2252af9e702c9047dba3bfc357f90e6c0876f55
Emilly <rodriguess.emilly@gmail.com>
2025-06-12 16:51:42 -0300

    adicionando novas frases no biscoito da sorte

--- fortunes.js
+++ fortunes.js
@@ -1,7 +1,12 @@
 export const fortunes = [
-  "Você terá um dia incrível!",
-  "Grandes oportunidades estão a caminho.",
-  "A sorte está do seu lado.",
-  "Seu esforço será recompensado em breve.",
-  "Hoje é um bom dia para começar algo novo.",
+  
+"Hoje é um bom dia para tomar uma boa decisão... ou não. A escolha é sua!",
+"Você tem o poder de mudar o mundo... ou de continuar dormindo.",
+"Se você não tem sorte, tente a sua inteligência.",
+"A vida é como uma caixa de bombons... cheia de surpresas que você não vai gostar.",
+"Se você está com problemas, peça ajuda aos seus amigos... ou tente de novo.",
+"Você vai encontrar o amor da sua vida... ou não. A vida continua.",
+"Se você acha que tem sorte, procure por uma loja de jogos de azar.",
+"Se você não tem dinheiro, come mais biscoitos da sorte.",
+"A sorte está ao seu lado.",
 ];
